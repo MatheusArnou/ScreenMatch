b615b8e1c9ae2cd60873eebcfddfb7c5da776ccd
MatheusArnou <matheusbispo28@gmail.com>
2025-10-30 20:30:18 -0300

    começando a trabalhar com decisores

---
+++ src/Condicional.java
@@ -0,0 +1,21 @@
+public class Condicional {
+    public static void main(String[] args) {
+        int anoDeLancamento = 2022;
+        boolean incluidoNoPlano = true;
+        double notaAvaliacao = 8.1;
+        String tipoPlano = "plus";
+
+        if(anoDeLancamento >= 2022){
+            System.out.println("Lançamentos que os clientes estão curtindo");
+        } else {
+            System.out.println("Filmes retrô que vale a pena assistir");
+        }
+        if(incluidoNoPlano || tipoPlano.equals("plus")){
+            System.out.println("Aproveite esse lançamento sem pagar a mais");
+        } else {
+            System.out.println("Para assistir esse filme você precisa adquirir ou assinar o plano Plus");
+        }
+
+
+    }
+}
