9d94a6b7fd54d239239217425e67af4c32a972d4
MatheusArnou <matheusbispo28@gmail.com>
2025-10-30 23:28:04 -0300

    Leitura de outros tipos de dados primitivos

--- src/Leitura.java
+++ src/Leitura.java
@@ -6,7 +6,16 @@ public class Leitura {
 
         System.out.println("Digite o nome do seu filme favorito");
         String filme = leitura.nextLine();
+        System.out.println("Qual o ano de lançamento?");
+        int anoDeLancamento = leitura.nextInt();
+        System.out.println("Qual sua avaliação do filme?");
+        double avaliacaoFilme = leitura.nextDouble();
+
         System.out.println(filme);
+        System.out.println(anoDeLancamento);
+        System.out.println(avaliacaoFilme);
+
+
 
     }
 }
