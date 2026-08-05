f64af7a9025d2e4421447c1a344299618120706c
MatheusArnou <matheusbispo28@gmail.com>
2026-08-05 18:28:08 -0300

    Aula trabalhando com textos

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,25 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Esse é o Screen Match");
         System.out.println("Filme: Top Gun: Maverick");
+        //System.out.println("\n");
 
         int anoDeLancamento = 2022;
         boolean incluidoNoPlano = true;
+        double notaDoFilme = (9.8+6.3+8.0) / 3;
+        String sinopse;
+        sinopse = """
+                  Filme Top Gun
+                  Filme de aventura com gala dos anos 80
+                  Muito bom!
+                  Ano de lançamento
+                """ + anoDeLancamento;
 
+
+        System.out.println("\n");
         System.out.println("Ano de lançameto: " + anoDeLancamento);
+        System.out.println("Nota do filme: " + notaDoFilme);
+        System.out.println(sinopse);
+
 
     }
 }
