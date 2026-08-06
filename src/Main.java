9241810b2062b2d7d0988c325871dabb8092df6a
MatheusArnou <matheusbispo28@gmail.com>
2026-08-05 22:22:53 -0300

    Trabalhando com casting, formatação de textos e textBlock

--- src/Main.java
+++ src/Main.java
@@ -4,24 +4,24 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Esse é o Screen Match");
         System.out.println("Filme: Top Gun: Maverick");
-        //System.out.println("\n");
-
         int anoDeLancamento = 2022;
         boolean incluidoNoPlano = true;
         double notaDoFilme = (9.8+6.3+8.0) / 3;
+        int classificacao = (int) notaDoFilme / 2;
         String sinopse;
         sinopse = """
                   Filme Top Gun
                   Filme de aventura com gala dos anos 80
+                  nota %.2f
                   Muito bom!
-                  Ano de lançamento
-                """ + anoDeLancamento;
+                  Ano de lançamento %d
+                """.formatted(notaDoFilme,anoDeLancamento);
 
-
-        System.out.println("\n");
         System.out.println("Ano de lançameto: " + anoDeLancamento);
-        System.out.println("Nota do filme: " + notaDoFilme);
+        System.out.println("Nota do filme: " + classificacao);
         System.out.println(sinopse);
+
+
 
 
     }
