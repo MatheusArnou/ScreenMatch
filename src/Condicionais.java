700e06236e483c7751a61bc2c1d34e0ee3d207c1
MatheusArnou <matheusbispo28@gmail.com>
2026-08-05 23:45:42 -0300

    Trabalando com IF

---
+++ src/Condicionais.java
@@ -0,0 +1,21 @@
+public class Condicionais {
+    public static void main(String[] args) {
+        int anoDeLancamento = 1990;
+        double notaDoFilme = (9.8 + 6.3 + 8.0) / 3;
+        boolean incluidoNoPlano = true;
+        String tipoPlano = "plus";
+
+        if (anoDeLancamento >= 2022) {
+            System.out.println("Lançamento que os clientes estão curtindo");
+        } else {
+            System.out.println("Filme retrô que vale a pena ver de novo!");
+        }
+
+        if(incluidoNoPlano || tipoPlano.equals("plus")){
+            System.out.println("Usuário pode assitir");
+        } else {
+            System.out.println("Para assitir pode acessar o nosso plano plus");
+        }
+
+    }
+}
