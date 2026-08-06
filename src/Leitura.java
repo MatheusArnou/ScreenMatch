25c097a501a6fac8a9fcf6ed7011282ce3d70e02
MatheusArnou <matheusbispo28@gmail.com>
2026-08-06 00:09:38 -0300

    Pegando informações do usuário com a classe Scanner

---
+++ src/Leitura.java
@@ -0,0 +1,14 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Leitura {
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+        System.out.println("Digite seu filme favorito: ");
+        String filme = leitor.nextLine();
+        System.out.println("Qual o ano de lançamento");
+        int anoDeLancamento = leitor.nextInt();
+        System.out.println("Avalie o filme: ");
+        double avaliacao = leitor.nextDouble();
+    }
+}
