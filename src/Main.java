7edf7ba8a93db05645baa2c34d94ff30a314f4a4
MatheusArnou <matheusbispo28@gmail.com>
2026-08-05 23:46:51 -0300

    Estrutura inicial do projeto

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,10 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Esse é o Screen Match");
         System.out.println("Filme: Top Gun: Maverick");
         int anoDeLancamento = 2022;
         boolean incluidoNoPlano = true;
-        double notaDoFilme = (9.8+6.3+8.0) / 3;
+        double notaDoFilme = (9.8 + 6.3 + 8.0) / 3;
         int classificacao = (int) notaDoFilme / 2;
         String sinopse;
         sinopse = """
@@ -15,13 +14,11 @@ public class Main {
                   nota %.2f
                   Muito bom!
                   Ano de lançamento %d
-                """.formatted(notaDoFilme,anoDeLancamento);
+                """.formatted(notaDoFilme, anoDeLancamento);
 
         System.out.println("Ano de lançameto: " + anoDeLancamento);
         System.out.println("Nota do filme: " + classificacao);
         System.out.println(sinopse);
-
-
 
 
     }
