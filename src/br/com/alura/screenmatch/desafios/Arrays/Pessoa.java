20aa3d88e9826909bcfb21f5e1fc24f6475bbc72
MatheusArnou <matheusbispo28@gmail.com>
2026-08-19 21:03:58 -0300

    Add: desafio ArrayList

---
+++ src/br/com/alura/screenmatch/desafios/Arrays/Pessoa.java
@@ -0,0 +1,4 @@
+package br.com.alura.screenmatch.desafios.Arrays;
+
+public class Pessoa {
+}
---
+++ src/br/com/alura/screenmatch/desafios/Arrays/Principal.java
@@ -0,0 +1,7 @@
+package br.com.alura.screenmatch.desafios.Arrays;
+
+public class Principal {
+    public static void main(String[] args) {
+
+    }
+}
